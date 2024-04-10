e828d58bafca8e7c6deef4c8e1520e4857979849
dir-FY24yanagasekeigo <keigo.yanagase.k338@dirbato.co.jp>
2024-04-10 15:23:59 +0900

    レッスン６完了

---
+++ Test/src/calc/CalculatorService.java
@@ -0,0 +1,23 @@
+package calc;
+
+import org.junit.validator.PublicClassValidator;
+
+public class CalculatorService {
+    private Calculator calculator;
+    
+    public CalculatorService(Calculator calculator) {
+        this.calculator = calculator;
+    }
+        
+    public String addAndReport(int a, int b) {
+        int result = calculator.add(a, b);
+        return "Result : " + result;
+    }
+        
+    public String subtractAndReport(int a, int b) {
+        int result = calculator.subtract(a, b);
+        return "Result : " + result;
+    }
+    
+
+}
